5b9db420a62d71040c60f23e148b6791575037c6
AmalPAnil200 <amalpanilebsgl@gmail.com>
2026-04-02 15:12:48 +0530

    host

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,26 @@
+//const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     dialect: 'mysql',
+//     logging: false, // Optional: cleans up your console
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   }
+// );
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -7,7 +30,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
+    dialect: 'postgres',
     logging: false, // Optional: cleans up your console
     pool: {
       max: 5,
