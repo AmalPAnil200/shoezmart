812303f59904641e2c3f333875262eb417d7a8eb
AmalPAnil200 <amalpanilebsgl@gmail.com>
2026-04-02 17:00:33 +0530

    host

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,25 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     dialect: 'mysql',
+//     logging: false, // Optional: cleans up your console
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   }
+// );
+
+//module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
@@ -6,38 +28,16 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
-    logging: false, // Optional: cleans up your console
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // important for Render
+      },
+    },
+  },
 );
 
 module.exports = sequelize;
-
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: "postgres",
-//     logging: false,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false, // important for Render
-//       },
-//     },
-//   },
-// );
-
-// module.exports = sequelize;
